75c0f645e82f4603fe1347201f975c216b4e1011
jm777trader <gabrielpereiraa0604@gmail.com>
2025-05-19 23:28:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let index = 0;
+const testimonials = document.querySelectorAll(".testimonial");
+setInterval(() => {
+  testimonials[index].classList.remove("active");
+  index = (index + 1) % testimonials.length;
+  testimonials[index].classList.add("active");
+}, 4000);
