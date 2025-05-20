08469caa9b41e7db217b2122ee09094f6a19bc7a
jm777trader <gabrielpereiraa0604@gmail.com>
2025-05-20 01:19:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-
-// Código para interações futuras (animações, scroll, etc.)
+let index = 0;
+const testimonials = document.querySelectorAll(".testimonial");
+setInterval(() => {
+  testimonials[index].classList.remove("active");
+  index = (index + 1) % testimonials.length;
+  testimonials[index].classList.add("active");
+}, 4000);
